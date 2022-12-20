fe614eaa2d116394382f29a147bda1f652e6c0cf
Mohamad SababSaz <msabab95@gmail.com>
2022-12-20 18:52:34 +0330

    initialize react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import ErrorPage from './error-page'
 import './index.css'
+import Contact from './routes/contact'
+import Root from './routes/root'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "view/:contactId",
+    element: <Contact />,
+  },
+  {
+    path: "edit/:contactId",
+    element: <Contact />,
+  },
+  {
+    path: "new",
+    element: <Contact />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
